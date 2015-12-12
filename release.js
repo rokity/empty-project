391765fe7f6948e7c9b9c0c608c872b017b6901a
Riccardo Amadio <laemaildiriccardo@gmail.com>
2015-12-12 06:03:52 -0500

    change port

--- release.js
+++ release.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 var options = {};
 
 // options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
+ options.port = parseInt(8080);
 // options.config = { name: 'total.js' };
 // options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
 // options.sleep = 2000;
